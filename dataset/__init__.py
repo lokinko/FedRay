c62c67a6a270b0e527bef668886c9da986c6c3ba
lokinko <qxm310556100@gmail.com>
2024-12-23 21:26:28 +0800

    typo(text): add gitignore and init file.

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,4 +1,4 @@
 from dataset.base_dataset import BaseDataset
-from dataset.base_dataset import DatasetFactory
+from dataset.dataset_factory import DatasetFactory
 
 __all__ = ['BaseDataset', 'DatasetFactory']
